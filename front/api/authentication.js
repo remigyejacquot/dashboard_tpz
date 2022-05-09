af047061163d0b950fc13c0823cb616e1f9bdbe3
TessMachd <tessmchd@hotmail.fr>
2022-05-09 09:45:38 +0200

    env url

--- front/api/authentication.js
+++ front/api/authentication.js
@@ -2,11 +2,11 @@
 import axiosInstance from "./axiosInstance";
 
 async function login(data) {
-  return await axiosInstance.post("/tpz/dashboard_tpz/back/public/index.php/authentication", data);
+  return await axiosInstance.post(process.env.VUE_BASE_URL + "/authentication", data);
 }
 
 async function logout() {
-  return await axiosInstance.post("/tpz/dashboard_tpz/back/public/index.php/logout");
+  return await axiosInstance.post(process.env.VUE_BASE_URL + "logout");
 }
 
 export { login, logout };
