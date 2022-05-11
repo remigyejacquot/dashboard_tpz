933a8d42fe2762b09e6b9c706a0af8924d3ebf4c
Rémi GYEJACQUOT <r.gyejacquot@maatdata.fr>
2022-05-11 09:28:47 +0200

    fix title change when toggle licence

--- front/src/views/Dashboard.vue
+++ front/src/views/Dashboard.vue
@@ -3,9 +3,22 @@
     <h1>DASHBOARD</h1>
     <p>USER</p>
     <b-card-text>{{ user }}</b-card-text>
-    <p>TOUTES LES AGENCES DE LA LICENCE {{user.is_dev ? 'DEV' : 'COM'}}</p>
+    <p>TOUTES LES AGENCES DE LA LICENCE {{type === 'dev' ? 'DEV' : 'COM'}}</p>
     <b-button @click="toggleLicence">CHANGER DE LICENCE</b-button>
-    <b-card-text v-for="agency in agencies" :key="agency.id">{{ agency }}</b-card-text>
+    <div v-if="type === 'dev'">
+      <b-card-text
+        v-for="agency in devAgencies"
+        :key="agency.id"
+        >{{ agency }}</b-card-text
+      >
+    </div>
+    <div v-else>
+      <b-card-text
+          v-for="agency in comAgencies"
+          :key="agency.id"
+      >{{ agency }}</b-card-text
+      >
+    </div>
     <p>TPZMEMBERS</p>
     <b-card-text v-for="member in tpzMembers" :key="member.id">{{ member }}</b-card-text>
     <b-button :to="{name: 'Projects'}">LES PROJETS</b-button>
@@ -13,8 +26,7 @@
 </template>
 
 <script>
-import {getLicenceAgencies, getUserLicenceAgencies} from "../../api/agencies";
-import {getAllMembers} from "../../api/tpzMembers";
+import {getAllTpzMembers} from "../../api/tpzMembers";
 import {getUser} from "../../api/users";
 import {getTpz} from "../../api/tpzs";
 
@@ -23,52 +35,40 @@ export default {
   data() {
     return {
       user:{},
-      agencies:[],
+      devAgencies:[],
+      comAgencies:[],
       tpzMembers:[],
-      agenciesType:"",
+      type:"",
     };
   },
   created() {
     const userId = JSON.parse(localStorage.getItem("user")).id
-    this.fetchUserInfo(userId)
+    this.fetchInfo(userId)
     this.fetchTpzMembers()
-    this.fetchUserLicenceAgencies(userId)
   },
   methods: {
-    fetchUserInfo(id) {
+    fetchInfo(id) {
       getUser(id).then((res)=>{
         console.log(res.data.tpzId)
         this.user = res.data
+        this.type = res.data.is_dev ? 'dev' : 'com'
         //localStorage.setItem("user", JSON.stringify(res.data)) // store the user in localstorage
-        this.agenciesType = res.data.is_dev ? 'dev' : 'com'
         getTpz(res.data.tpzId).then(res=>{
-          console.log('toto',res.data)
+          console.log('toto',res.data.agencies)
+          this.devAgencies = res.data.agencies.filter(el=>el.is_dev === true)
+          this.comAgencies = res.data.agencies.filter(el=>el.is_dev !== true)
         })
       })
     },
     fetchTpzMembers() {
-       getAllMembers().then((res)=>{
+      getAllTpzMembers().then((res)=>{
          res.data['hydra:member'].forEach(member => {
            this.tpzMembers.push(member)
          })
        })
     },
-    fetchUserLicenceAgencies(id) {
-      getUserLicenceAgencies(id).then((res)=>{
-         res.data.forEach(agency=>{
-           this.agencies.push(agency)
-         })
-      })
-    },
     toggleLicence() {
-      let newAgencies = []
-      this.agenciesType === 'dev' ? this.agenciesType = 'com' : this.agenciesType ='dev'
-      getLicenceAgencies(this.agenciesType).then((res)=>{
-        res.data.forEach(agency=>{
-          newAgencies.push(agency)
-        })
-        this.agencies = newAgencies
-      })
+      this.type === 'dev' ? this.type = 'com' : this.type = 'dev'
     }
   },
 };
