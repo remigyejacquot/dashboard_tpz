a96739d935957b00185afc007077f1ed025d50d6
Rémi GYEJACQUOT <r.gyejacquot@maatdata.fr>
2022-05-12 15:59:01 +0200

    toggle licence button style when clicked

--- front/src/views/GestionEtudiants.vue
+++ front/src/views/GestionEtudiants.vue
@@ -1,55 +1,89 @@
 <template>
   <div>
-<!--    Liste des étudiants-->
+    <!--    Liste des étudiants-->
     <section>
       <div class="mt-3">
         <b-button-group>
-          <b-button @click="toggleLicence">DEV</b-button>
-          <b-button @click="toggleLicence">COM</b-button>
+          <b-button id="dev" @click="toggleLicence($event)" :style="style.dev">DEV</b-button>
+          <b-button id="com" @click="toggleLicence($event)" :style="style.com"
+            >COM</b-button
+          >
         </b-button-group>
         <b-card-text>Liste des étudiants</b-card-text>
-        <b-icon icon="plus-circle-fill" style='color: #F96197;' font-scale="1.5" @click="addStudent">+</b-icon>
+        <b-icon
+          icon="plus-circle-fill"
+          style="color: #f96197"
+          font-scale="1.5"
+          @click="addStudent"
+          >+</b-icon
+        >
       </div>
     </section>
-<!--    Recherche-->
+    <!--    Recherche-->
     <section>
-      <autocomplete-users v-bind:options="suggestions"  input="etudiant" placeholder="Rechercher un étudiant"></autocomplete-users>
+      <autocomplete-users
+        v-bind:options="suggestions"
+        input="etudiant"
+        placeholder="Rechercher un étudiant"
+      ></autocomplete-users>
     </section>
   </div>
 </template>
 
-
 <script>
-import {getUsers} from "../../api/users";
+import { getUsers } from "../../api/users";
 import AutocompleteUsers from "../components/autocompleteUsers";
+import {COLORS} from "../utils/colors";
 
 export default {
   name: "GestionEtudiants",
   components: { AutocompleteUsers },
   data() {
-    return{
-      suggestions:{}
-    }
+    return {
+      suggestions: {},
+      type: "dev",
+      style: {
+        dev: "background-color: "+COLORS.yellow,
+        com: "",
+      },
+    };
   },
   mounted() {
-    this.getAllStudents()
+    this.getAllStudents();
   },
-  methods:{
-    toggleLicence() {
-      console.log('change licence')
+  methods: {
+    toggleLicence(e) {
+      console.log(e.target.id)
+      /*si on clique sur DEV et que COM était sélectionné on switch sinon rien*/
+      if (e.target.id === 'dev'){
+        if(this.type === 'com'){
+          this.type = 'dev'
+          this.style = {
+            dev: "background-color: "+COLORS.yellow,
+            com: ""
+          }
+        }
+      /*si on clique sur COM et que DEV était sélectionné on switch sinon rien*/
+      } else if (e.target.id === 'com') {
+        if(this.type === 'dev'){
+          this.type = 'com'
+          this.style = {
+            dev: "",
+            com: "background-color: "+COLORS.yellow,
+          }
+        }
+      }
     },
     addStudent() {
-      console.log('add new student')
+      console.log("add new student");
     },
     getAllStudents() {
-      getUsers(JSON.parse(localStorage.getItem("tpzId"))).then(res=>{
-        console.log(res.data)
-      })
-    }
-  }
-}
+      getUsers(JSON.parse(localStorage.getItem("tpzId"))).then((res) => {
+        console.log(res.data);
+      });
+    },
+  },
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
