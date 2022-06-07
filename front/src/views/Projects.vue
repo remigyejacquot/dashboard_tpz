c4177b4ae3755b06a010a4517e144026c1e34a4d
Rémi GYEJACQUOT <r.gyejacquot@maatdata.fr>
2022-06-07 21:20:47 +0200

    add progress bar and show agency content

--- front/src/views/Projects.vue
+++ front/src/views/Projects.vue
@@ -3,37 +3,73 @@
     <navSidebar></navSidebar>
     <main class="container bg-light col-10 px-3">
       <section
-          class="title-section d-flex justify-content-between align-items-center flex-basis-50"
+          class="title-section d-flex justify-content-between align-items-center"
       >
         <div class="d-flex header-section-div">
           <h2>Project Flyer</h2>
-          <p>Barre de progression</p>
+          <b-progress :max="progress.max" variant="warning" striped show-progress animated>
+            <b-progress-bar :value="progress.value" :label="`${progress.value}%`" style="color: black"/>
+          </b-progress>
         </div>
-        <div class="d-flex header-section-div">
-          <p>5</p>
-          <button class="modify-btn">Modifier</button>
+        <div class="d-flex header-section-div justify-content-between align-items-end">
+          <b-icon
+              icon="plus-circle-fill"
+              style="color: #f96197"
+              font-scale="1.5"
+              @click="()=>{}"
+              class="icon-btn"
+          >+</b-icon>
+          <div>
+            <b-icon
+                icon="pencil-fill"
+                style="color: #f7b000"
+                font-scale="1.2"
+                @click="()=>{}"
+                class="icon-btn"/>
+            <button class="modify-btn">Modifier</button>
+          </div>
         </div>
       </section>
       <section
-          class="switch-section d-flex justify-content-between align-items-center flex-basis-50"
+          class="switch-section d-flex justify-content-between align-items-center"
       >
         <div class="d-flex header-section-div">
-          <h2>Entreprise machin</h2>
+          <h2>{{agency.name}}</h2>
           <ul id="example-1">
             <li v-for="member in agency.users" :key="member.id">
+              <div>
               {{ member.firstname }}
               {{ member.lastname }}
+                <font-awesome-icon class="crown-icon" icon="fa-solid fa-crown" v-if="member.tpz_role.find(el=>el.role === 'chef d\'agence')"/>
+              </div>
             </li>
           </ul>
         </div>
-        <div class="d-flex header-section-div">
-          <p>5</p>
-          <button class="modify-btn">Modifier</button>
+        <div class="d-flex header-section-div justify-content-between align-items-end">
+          <b-card-text class="d-flex" style="color: #4D3677">
+          <p>{{ agency.users.length }}</p>
+          <b-icon
+              icon="people-fill"
+              style="color: #4D3677"
+              font-scale="1.2"
+              @click="()=>{}"
+              class="icon-btn mx-1"/>
+          </b-card-text>
+          <div>
+            <b-icon
+                icon="pencil-fill"
+                style="color: #f7b000"
+                font-scale="1.2"
+                @click="()=>{}"
+                class="icon-btn"
+            >+</b-icon>
+            <button class="modify-btn">Modifier</button>
+          </div>
         </div>
       </section>
       <h1>PROJECTS</h1>
       <p>USER</p>
-      <b-card-text>{{ user }}</b-card-text>
+      <b-card-text>{{ agency }}</b-card-text>
     </main>
   </div>
 </template>
@@ -49,7 +85,13 @@ export default {
     return {
       user: {},
       projects: [],
-      agency: {},
+      agency: {
+        users:[]
+      },
+      progress: {
+        value:40,
+        max:100
+      },
     };
   },
   created() {
@@ -72,8 +114,14 @@ export default {
 </script>
 
 <style scoped>
-.flex-basis-50 {
-  flex-basis: 50%;
+.container {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-template-rows: auto;
+  grid-template-areas:
+    "list search"
+    "list add";
+  justify-items: stretch;
 }
 
 main {
@@ -89,11 +137,13 @@ main > section {
 }
 
 .header-section-div {
-  align-content: center;
   flex-direction: column;
-  justify-content: space-between;
   height: 100%;
   padding: 1em;
+}
+
+li {
+  list-style: none;
 }
 
 .modify-btn {
