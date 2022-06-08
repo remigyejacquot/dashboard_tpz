1c597da3c0faa893c38db6f7e75bb703f19ecfee
Rémi GYEJACQUOT <r.gyejacquot@maatdata.fr>
2022-06-08 21:02:15 +0200

    retrieve and display each task

--- front/src/views/Projects.vue
+++ front/src/views/Projects.vue
@@ -5,20 +5,22 @@
       <section
           class="title-section d-flex justify-content-between align-items-center"
       >
-        <div class="d-flex header-section-div">
-          <h2>Project Flyer</h2>
-          <b-progress :max="progress.max" variant="warning" striped show-progress animated>
-            <b-progress-bar :value="progress.value" :label="`${progress.value}%`" style="color: black"/>
+        <div class="d-flex header-section-div flex-column">
+          <b-form-select v-model="selected" :options="projects" text-field="text" value-field="value" class="mb-2"></b-form-select>
+          <div class="d-flex w-100 justify-content-between align-items-center">
+          <b-progress :max="progress.max" variant="warning" striped animated class="w-100 me-2">
+            <b-progress-bar :value="progress.value"/>
           </b-progress>
+            <p class="m-0">{{progress.value}}%</p>
+          </div>
         </div>
-        <div class="d-flex header-section-div justify-content-between align-items-end">
+        <div class="d-flex header-section-div flex-column justify-content-between align-items-end">
           <b-icon
               icon="plus-circle-fill"
               style="color: #f96197"
               font-scale="1.5"
               @click="()=>{}"
-              class="icon-btn"
-          >+</b-icon>
+              class="icon-btn"/>
           <div>
             <b-icon
                 icon="pencil-fill"
@@ -33,7 +35,7 @@
       <section
           class="switch-section d-flex justify-content-between align-items-center"
       >
-        <div class="d-flex header-section-div">
+        <div class="d-flex header-section-div flex-column">
           <h2>{{agency.name}}</h2>
           <ul id="example-1">
             <li v-for="member in agency.users" :key="member.id">
@@ -45,10 +47,10 @@
             </li>
           </ul>
         </div>
-        <div class="d-flex header-section-div justify-content-between align-items-end">
+        <div class="d-flex header-section-div flex-column justify-content-between align-items-end">
           <b-card-text class="d-flex" style="color: #4D3677">
           <p>{{ agency.users.length }}</p>
-          <b-icon
+            <b-icon
               icon="people-fill"
               style="color: #4D3677"
               font-scale="1.2"
@@ -61,15 +63,66 @@
                 style="color: #f7b000"
                 font-scale="1.2"
                 @click="()=>{}"
-                class="icon-btn"
-            >+</b-icon>
+                class="icon-btn"/>
             <button class="modify-btn">Modifier</button>
           </div>
         </div>
       </section>
-      <h1>PROJECTS</h1>
-      <p>USER</p>
-      <b-card-text>{{ agency }}</b-card-text>
+      <section class="d-flex flex-column">
+        <div class="d-flex header-section-div h-auto justify-content-between w-100">
+          <h2>Tâches</h2>
+          <b-icon
+              icon="plus-circle-fill"
+              style="color: #f96197"
+              font-scale="1.5"
+              @click="()=>{}"
+              class="icon-btn"/>
+        </div>
+        <div>
+          <table>
+            <tr v-for="task in tasks" :key="task['@id']" @click="setCurrentTask(task)">
+              <td class="w-100">
+                <div class="d-flex align-items-center justify-content-between">
+                  <div class="d-flex align-items-center">
+                  <b-icon
+                      v-if="task.is_finished"
+                      icon="check"
+                      style="color: green"
+                      font-scale="2"
+                      @click="()=>{}"
+                      class="icon-btn"
+                  />
+                  <b-icon
+                      v-else
+                      icon="dash"
+                      style="color: black"
+                      font-scale="2"
+                      @click="()=>{}"
+                      class="icon-btn"
+                  />
+                  {{task.title}}
+                  </div>
+                  <div>
+                    <b-icon
+                        icon="plus-circle-fill"
+                        style="color:#FAD26E"
+                        font-scale="1.5"
+                        @click="()=>{}"
+                        class="icon-btn"/>
+                  </div>
+                </div>
+              </td>
+              <td>{{new Date(task.updated_at).toLocaleDateString()}}</td>
+            </tr>
+          </table>
+        </div>
+      </section>
+      <section>
+        {{currentTask.title}}
+        {{currentTask.description}}
+        {{currentTask.subtasks}}
+      </section>
+      <b-card-text>{{ projects }}</b-card-text>
     </main>
   </div>
 </template>
@@ -84,12 +137,15 @@ export default {
   data() {
     return {
       user: {},
-      projects: [],
       agency: {
         users:[]
       },
+      selected: {},
+      projects: [],
+      tasks:[],
+      currentTask:{},
       progress: {
-        value:40,
+        value:0,
         max:100
       },
     };
@@ -99,16 +155,34 @@ export default {
     this.user = user
     this.getAllAgencyMembers(user["agency"].substr(-1)); //retrieve user index from @id
   },
+  watch: {
+    selected(currentProject) {
+      const totalProgress = (currentProject.tasks.filter(el=>el.is_finished === true).length/currentProject.tasks.length)*100
+      this.progress.value = totalProgress
+      this.tasks=[]
+      currentProject.tasks.forEach((task)=>{
+        this.tasks.push(task)
+      })
+      this.currentTask = currentProject.tasks.shift()
+    }
+  },
   methods: {
     getAllAgencyMembers(id) {
       getAgencyMembers(id).then((res) => {
-        console.log(res.data)
         this.agency = res.data;
         res.data.projects.forEach((project) => {
-          this.projects.push(project);
+          //Useful to display in the select
+          this.projects.push({
+            value:project,
+            text:project.name
+          });
         });
+        this.selected = res.data.projects.shift()
       });
     },
+    setCurrentTask(task) {
+      this.currentTask = task
+    }
   },
 };
 </script>
@@ -136,8 +210,11 @@ main > section {
   border-radius: 10px;
 }
 
+h2 {
+  color: #FAD26E;
+}
+
 .header-section-div {
-  flex-direction: column;
   height: 100%;
   padding: 1em;
 }
@@ -150,5 +227,14 @@ li {
   color: #f7b000;
   background: none;
   border: none;
+}
+
+td {
+  border: 1px solid black;
+  padding: .5em
+}
+
+tr:hover {
+  background-color: #FAD26E;
 }
 </style>
