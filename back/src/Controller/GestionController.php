c61539d5a44445e056aaa0b1d4e15299e864e8db
TessMachd <tessmchd@hotmail.fr>
2022-06-13 11:04:15 +0200

    clean file

--- back/src/Controller/GestionController.php
+++ back/src/Controller/GestionController.php
@@ -11,7 +11,6 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\SerializerInterface;
